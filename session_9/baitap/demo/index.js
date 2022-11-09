48ac78f1d3b2dc7df980ebf919fcf5c3351d5933
LeHoang <lehoang999113@gmail.com>
2022-11-09 21:43:17 +0700

    update

---
+++ session_9/baitap/demo/index.js
@@ -0,0 +1,11 @@
+function showAlert() {
+    alert("Hello world on alert!");
+}
+
+function showPrompt() {
+    prompt("Hello world on fromt!");
+}
+
+function showConfirm() {
+    confirm("Hello world on confirm!");
+}
